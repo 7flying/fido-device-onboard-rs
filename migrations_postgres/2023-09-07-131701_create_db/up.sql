0559933fba9acda572deb6199439566170cbd800
Irene Diez <idiez@redhat.com>
2023-09-29 17:30:25 +0200

    update

--- migrations_postgres/2023-09-07-131701_create_db/up.sql
+++ migrations_postgres/2023-09-07-131701_create_db/up.sql
@@ -6,17 +6,17 @@ CREATE TABLE ownership_voucher (
 );
 
 CREATE TABLE manufacturer_vouchers (
-    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
+    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE
 );
 
-CREATE TABLE owner_vouchers (
-    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
-    to2_performed boolean,
-    to0_accept_owner_wait_seconds bigint
-);
+-- CREATE TABLE owner_vouchers (
+--     ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
+--     to2_performed boolean,
+--     to0_accept_owner_wait_seconds bigint
+-- );
 
-CREATE TABLE rendezvous_vouchers (
-    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
-    ttl bigint
-);
+-- CREATE TABLE rendezvous_vouchers (
+--     ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
+--     ttl bigint
+-- );
 
