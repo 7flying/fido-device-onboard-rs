0d10c0d3eece89b34a493e82de70015fa48eb8df
Irene Diez <idiez@redhat.com>
2023-09-29 17:29:38 +0200

    update

--- migrations_postgres/2023-09-07-131701_create_db/up.sql
+++ migrations_postgres/2023-09-07-131701_create_db/up.sql
@@ -6,18 +6,17 @@ CREATE TABLE ownership_voucher (
 );
 
 CREATE TABLE manufacturer_vouchers (
-    ov_guid references ownership_voucher(guid) PRIMARY KEY ON DELETE CASCADE
+    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
 );
 
 CREATE TABLE owner_vouchers (
---    id bigserial NOT NULL PRIMARY KEY,
-    ov_guid references ownership_voucher(guid) PRIMARY KEY ON DELETE CASCADE,
+    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
     to2_performed boolean,
     to0_accept_owner_wait_seconds bigint
 );
 
 CREATE TABLE rendezvous_vouchers (
-    ov_guid references ownership_voucher(guid) PRIMARY KEY ON DELETE CASCADE,
+    ov_guid varchar(36) PRIMARY KEY references ownership_voucher(guid) ON DELETE CASCADE,
     ttl bigint
 );
 
